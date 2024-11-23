c12f8c88073dcc4d32807288ab879f4b0d687ac8
Travis Powell <trapowell86@gmail.com>
2024-11-23 12:45:07 -0600

    first commit

---
+++ src/main/java/projects/ProjectsApp.java
@@ -0,0 +1,18 @@
+package projects;
+
+import projects.dao.DbConnection;
+
+/*
+* @author Promineo
+*
+*/
+public class ProjectsApp {
+	
+/**
+ * @param args
+ */
+	
+	public static void main (String[] args) {
+		DbConnection.getConnection();
+	}
+}
